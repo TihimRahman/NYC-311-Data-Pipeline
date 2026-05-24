ea05ef168843f82e0be78559eb69828a4899e15f
TihimRahman <tihimrahman777@gmail.com>
2026-05-24 22:36:11 +0100

    fetched_batch testing[0]

--- ntc311_extractor.py
+++ ntc311_extractor.py
@@ -3,7 +3,13 @@ import logging
 import os
 from datetime import datetime
 
-#CONFIG
+import requests
+import json
+import time
+
+#                                   CONFIG
+
+
 BASE_URL    = "https://data.cityofnewyork.us/resource/erm2-nwe9.json"
 BATCH_SIZE  = 1000
 MAX_RECORDS = 10000
@@ -14,7 +20,13 @@ RETRY_DELAY = 5
 S3_BUCKET   = "your-bucket-name"
 S3_PREFIX   = "raw/nyc311"
 
-#LOGGING SETUP
+
+
+
+
+
+#                                           LOGGING SETUP
+
 
 LOG_DIR = "logs"
 os.makedirs(LOG_DIR, exist_ok=True)
@@ -32,30 +44,86 @@ logger=logging.getLogger(__name__)
 
 
 
-#FETCH SINGLE BATCH
-def fetch_batch():
-    pass
 
-#DATA QUALITY
+
+
+
+#                                               FETCH SINGLE BATCH
+
+
+def fetch_batch(session: requests.Session, offset: int) -> list:
+    params = {
+        "$limit": BATCH_SIZE,
+        "$offset": 0,
+        "$order": "created_date DESC"
+    }
+
+    logger.info(f"Fetching batch | offset {offset}")
+
+    response = session.get(BASE_URL, params=params, timeout=30)
+    response.raise_for_status()
+    records = response.json()
+    logger.info(f"Fetched {len(records)} records | offset {offset}")
+
+    return records
+
+
+
+
+
+
+
+
+
+
+
+
+#                                                  DATA QUALITY
+
+
 def check_data_quality():
     pass
 
-#SAVE BATCH TO S3
+
+
+
+
+
+
+
+#                                                 SAVE BATCH TO S3
+
 def save_to_s3():
     pass
 
-#ORCHESTRATOR
+
+
+
+
+
+
+#                                                  ORCHESTRATOR
+
+
+
 def run_extraction():
     pass
 
 
-#ENTRY POINT
+
+
+
+
+
+#                                                   ENTRY POINT
+
+
 
 if __name__ == "__main__":
-    logger.info("Pipeline info")
-    logger.warning("Pipeline Warning")
-    logger.error("Pipeline Error")
-    pass
+    
+    with requests.Session() as session:
+        records = fetch_batch(session, offset=0)
+        print(records[0])
 
 
 
